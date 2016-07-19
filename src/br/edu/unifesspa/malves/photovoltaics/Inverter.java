8b320ee9742fce19c094269824c90acbd57674eb
marcelaalves89 <marcelaalves89@gmail.com>
2016-07-19 13:01:35 -0300

    Alterações 106-07-19 13:01

--- src/br/edu/unifesspa/malves/photovoltaics/Inverter.java
+++ src/br/edu/unifesspa/malves/photovoltaics/Inverter.java
@@ -9,17 +9,17 @@ package br.edu.unifesspa.malves.photovoltaics;
 public class Inverter {
 
 	/**
-	 * Inverter Acquisition Cost (in Brazilian Real R$)
+	 * Inverter Acquisition Cost (Brazilian Real - BRL)
 	 */
-	public static final double custo = 3679.0;
+	public static final double custo = 2846.37;
 	
 	/**
 	 * Inverter Efficiency
 	 */
-	public static final double eficiencia = 0.968;
+	public static final double eficiencia = 0.978;
 	
 	/**
-	 * Rated DC Input Power
+	 * Rated DC Input Power [kW]
 	 */
-	public static final double potenciaNominalEntrada = 4.375;
+	public static final double potenciaNominalEntrada = 5.0;
 }
